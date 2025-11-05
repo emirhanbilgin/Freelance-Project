f86373f87cfa9b99eda0c7f7b0eada9c0a78fb05
emirhanbilgin <emirhan.bilgin@bahcesehir.edu.tr>
2025-11-05 14:00:16 +0300

    Fix: Product modeline fillable property eklendi

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'default_price'];
+
     public function receiptItems()
     {
         return $this->hasMany(\App\Models\ReceiptItem::class);
